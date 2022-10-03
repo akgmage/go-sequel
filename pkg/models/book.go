86570b4cc3718b5561f87c00adde9cc86cb76249
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-03 23:18:41 +0530

    add CreateBook, GetAllBooks, GetBookById & DeleteBook method

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -22,3 +22,23 @@ func init() {
 	db.AutoMigrate(&Book{})
 }
 
+func(b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+func GetBookById(id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+func DeleteBook(Id int64) Book {
+	var book Book
+	db.Where("ID=?", Id).Delete(book)
+	return book
+}
