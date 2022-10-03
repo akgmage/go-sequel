78be09fff441ce9e16fa0523a087edf54ada00c2
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-03 22:41:09 +0530

    register routes

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/akgmage/go-sequel/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
