1dd05188cef27e42b4a268b70db6a951750e7d8b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-03 23:17:20 +0530

    add main.go

---
+++ cmd/main/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/akgmage/go-sequel/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("Localhost:8000", r))
+}
