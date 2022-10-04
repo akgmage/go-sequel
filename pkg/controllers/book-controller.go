03013f53fa7531b58372b1569762d57334009b49
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-04 22:21:59 +0530

    add CreateBook method

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/akgmage/go-sequel/pkg/models"
+	"github.com/akgmage/go-sequel/pkg/utils"
 	"github.com/gorilla/mux"
 )
 
@@ -20,7 +21,7 @@ func GetBook(w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 }
 
-func GetBookById(w http.ResponseWriter, r * http.Request) {
+func GetBookById(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	bookId := vars["bookId"]
 	ID, err := strconv.ParseInt(bookId, 0, 0)
@@ -31,4 +32,11 @@ func GetBookById(w http.ResponseWriter, r * http.Request) {
 	res, _ := json.Marshal(bookDetails)
 	w.Header().Set("Content-Type", "pkglication/json")
 	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.PareseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
 }
