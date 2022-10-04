a8adccc9d0c407986dfb537ca8ffded65a8b52e9
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-04 22:17:16 +0530

    add GetBookById method whish will return particular book

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -2,9 +2,12 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/akgmage/go-sequel/pkg/models"
+	"github.com/gorilla/mux"
 )
 
 var NewBook models.Book
@@ -12,7 +15,20 @@ var NewBook models.Book
 func GetBook(w http.ResponseWriter, r *http.Request) {
 	newBooks := models.GetAllBooks()
 	res, _ := json.Marshal(newBooks)
-	w.Header().Set("COntent-Type", "pkg/publication/json")
+	w.Header().Set("COntent-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r * http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
 	w.Write(res)
 }
